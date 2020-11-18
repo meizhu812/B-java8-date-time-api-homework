fcbcb0312fde51bd206a3b92a1ad5795aa485221
Zhu Mei <zhu.mei1@thoughtworks.com>
2020-11-18 09:00:56 +0800

    completed practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -9,8 +13,16 @@ import java.time.LocalDate;
  * @create 2020-05-15_16:33
  */
 public class Practice1 {
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        int currentYear = date.getYear();
+        LocalDate labourDayOfCurrentYear = getLabourDayOfYear(currentYear);
+        LocalDate nextLabourDay = date.isBefore(labourDayOfCurrentYear)
+                ? labourDayOfCurrentYear
+                : getLabourDayOfYear(currentYear + 1);
+        return ChronoUnit.DAYS.between(date, nextLabourDay);
+    }
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    private static LocalDate getLabourDayOfYear(int year) {
+        return LocalDate.of(year, Month.MAY, 1);
+    }
 }
